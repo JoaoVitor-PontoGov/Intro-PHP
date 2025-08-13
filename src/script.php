8896fcfe8e4181010c5dc757421c202ecb271d54
JoaoVitor-PontoGov <joaovitor@pontogovsistemas.com.br>
2025-08-13 14:52:15 -0300

    feat: Calculo de idade em 10 anos e verificação se é maior de idade

--- src/script.php
+++ src/script.php
@@ -4,11 +4,20 @@ $nome = $_POST["dsNome"];
 $idade = $_POST["dsIdade"];
 $genero = "";
 
-if ($_POST["dsGenero"] == "M") 
+if ($_POST["flGenero"] == "M") 
   $genero = "Masculino";
-elseif ($_POST["dsGenero"] == "F")
+elseif ($_POST["flGenero"] == "F")
   $genero = "Feminino";
 else 
   $genero = "Outro";
 
-echo "Olá ". $nome;  
+echo "Nome: ". $nome . ", Idade: ". $idade . ", Genero: ". $genero . "\n";
+
+$idadeFutura = $idade + 10;
+
+echo "Em 10 anos voce tera: " . $idadeFutura . " anos \n";
+
+if($idade>=18)
+  echo "Voce e maior de idade \n";
+else
+  echo "voce nao e maior de idade";
